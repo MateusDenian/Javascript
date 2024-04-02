83186395b124038f99aca415ca57fd798b214398
MateusDenian <130771266+MateusDenian@users.noreply.github.com>
2024-04-02 12:32:55 -0300

    adiciona e mudando objetivos

--- Javascript - Projetos de vida MateusDf/main.js
+++ Javascript - Projetos de vida MateusDf/main.js
@@ -15,17 +15,30 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-06-06T00:00:00");
+const tempoObjetivo1 = new Date("2024-04-30T00:00:00");
+const tempoObjetivo2 = new Date("2024-06-06T00:00:00");
+const tempoObjetivo3 = new Date("2024-10-03T00:00:00");
+const tempoObjetivo4 = new Date("2024-04-05T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+
+
+for (let i=0; 1<contadores.length;i++){
+    contadores[1].textContent = calculaTempo(tempos[1]);
+}
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(tempoFinal/1000);
     let minutos = Math.floor(segundos/60);
-    let horas = Math.floor(minutos/24);
+    let horas = Math.floor(minutos/60);
     let dias = Math.floor(horas/24);
+   
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 } 
-
